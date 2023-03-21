d954fb82b22d4cada6e66c25ca789ee44dff17b5
ROXXEN <88208519+ROXXEN@users.noreply.github.com>
2023-03-21 18:33:18 +0900

    make python

---
+++ main.py
@@ -0,0 +1,31 @@
+def plus(a=0, b=0):
+    print(a + b)
+
+
+def times(a=0, b=0):
+    print(a * b)
+
+
+def minus(a=0, b=100):
+    print(a - b)
+
+
+def divide(a=1, b=1):
+    print(a / b)
+
+
+def power(a=0, b=0):
+    print(a ** b)
+
+
+plus(2, 3)
+minus(23, 20)
+times(22, 992)
+divide(60, 3)
+power(3, 22)
+
+plus(3)
+minus(23)
+times(22)
+divide(3)
+power(22)
